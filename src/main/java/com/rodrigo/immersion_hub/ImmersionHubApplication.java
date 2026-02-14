03862d4a3caccca9fba8f1460ac31e078859d40c
Rodrigo de Oliveira <rodrigooliveirareis98@gmail.com>
2026-02-14 19:12:44 -0300

    feat: Initialize project structure

---
+++ src/main/java/com/rodrigo/immersion_hub/ImmersionHubApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.immersion_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmersionHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmersionHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigo/immersion_hub/ImmersionHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.immersion_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmersionHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
