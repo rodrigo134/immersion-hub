9419e4a4fafe499fdc535f88adcaae8f00af886d
Rodrigo de Oliveira <rodrigooliveirareis98@gmail.com>
2026-02-14 19:19:09 -0300

    git commit -m Add health check endpoint

---
+++ src/main/java/com/rodrigo/immersion_hub/api/HealthyController.java
@@ -0,0 +1,16 @@
+package com.rodrigo.immersion_hub.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthyController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("ok");
+    }
+
+
+}
